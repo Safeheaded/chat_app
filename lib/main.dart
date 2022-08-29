5e9de6a4ff1308f6f8807f5393c5eb4a6ab3e050
Patryk Marczak <m.patryk@outlook.com>
2022-08-29 11:56:57 +0200

    firebase setup & auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/screens/auth_screen.dart';
 import 'package:chat_app/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,11 +19,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Chat',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const ChatScreen()
-    );
+        title: 'Flutter Chat',
+        theme: ThemeData(
+                primarySwatch: Colors.pink,
+                backgroundColor: Colors.pink,
+                buttonTheme: ButtonTheme.of(context).copyWith(
+                  buttonColor: Colors.pink,
+                  textTheme: ButtonTextTheme.primary,
+                ),
+                elevatedButtonTheme: ElevatedButtonThemeData(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.pink,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)))))
+            .copyWith(
+                colorScheme: ThemeData()
+                    .colorScheme
+                    .copyWith(secondary: Colors.deepPurple)),
+        home: const AuthScreen());
   }
 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -6,12 +7,35 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (ctx, index) => Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('This works'),
-              )),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('chats/SIKPWrus5bo8tnzpsFmE/messages')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (ctx, index) => Container(
+                        padding: const EdgeInsets.all(8),
+                        child: Text(snapshot.data!.docs[index]['text']),
+                      ));
+            } else {
+              return const Text('No messages yet');
+            }
+          }),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/SIKPWrus5bo8tnzpsFmE/messages')
+              .add({'text': 'This was added by clicking a button!'});
+        },
+      ),
     );
   }
 }
